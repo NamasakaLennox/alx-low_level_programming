762612f4ffd13784c9c1eb2dc292000f29b6ad52
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-16 11:59:45 -0800

    std error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+/**
+ * main - where execution begins
+ *
+ * Description: prints in the std error
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" \
+			- Dora Korpar, 2015-10-19\n");
+	return (1);
+}
+
