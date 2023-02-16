2c011029f168763bd41c462c003af8c418988a1e
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-16 02:28:25 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	/* Print using Puts */
+	char mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(mystr); /* writing the string to standard output */
+
+	return 0;
+}
+
