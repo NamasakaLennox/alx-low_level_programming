b629e2cc849da05c32d2aa8a424236fbf5b8c036
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-16 03:03:15 -0800

    betty nightmare

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,15 @@
 #include<stdio.h>
+
 /**
  * main - where the program should begin
  *
  * Description: prints \"Programming is like building a multilingual puzzle
  *
- * Return : terminate the program
+ * Return : Always 0 (Success)
  */
 int main(void) /* Description: returns 0 for success */
 {
-	/* Print using Puts */
-	char mystr[] = "\"Programming is like building a multilingual puzzle";
-
-	puts(mystr); /* writing the string to standard output */
-
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
