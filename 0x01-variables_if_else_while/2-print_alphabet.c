44ad8e52ce28d91c243d078c23f77b1fc65f0d7c
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-17 07:59:15 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main- where execution begins
+ *
+ * Description: print letters of the alphabet
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+}
