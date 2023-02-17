53e15a756ddc08fab5de36254a49ba60c7a050a0
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-17 09:56:05 -0800

    comma separated

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - where the program begins execution
+ *
+ * Description: prints all single digit numbers separated by a comma
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x < 9)
+		{
+			putchar(',');
+			putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
