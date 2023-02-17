9ce33de1da11b0ea98a6b89671af06300e0552f4
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-17 09:19:10 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - where the program begins execution
+ *
+ * Description: prints all hexadecimal numbers
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'e'; x++)
+	{
+		putchar(x)
+	}
+	putchar('\n');
+	return (0);
+}
