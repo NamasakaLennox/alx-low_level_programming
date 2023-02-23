b21253054f4c3ea5a9f5bcc7617db0178d68b02c
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-23 07:24:44 -0800

    print an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,31 +8,17 @@
  */
 void print_number(int n)
 {
+	/* if number is negative */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		n = -1 * n;
 	}
-	if (n < 10)
+
+	/* call function recursively */
+	if (n / 10)
 	{
-		_putchar(n + '0');
+		print_number(n / 10);
 	}
-	else if (n < 100)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else
-	{
-		_putchar(n / 1000 + '0');
-		_putchar((n % 1000) / 100 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
+	_putchar(n % 10 + '0');
 }
