ead391f535d005048acc61e8b7d5539e2f7fc03f
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-23 07:53:43 -0800

    recursive integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,17 +8,23 @@
  */
 void print_number(int n)
 {
+	unsigned int n1;
+
 	/* if number is negative */
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -1 * n;
+		n1 = -1 * n;
+	}
+	else
+	{
+		n1 = n;
 	}
 
 	/* call function recursively */
-	if (n / 10)
+	if (n1 / 10)
 	{
-		print_number(n / 10);
+		print_number(n1 / 10);
 	}
-	_putchar(n % 10 + '0');
+	_putchar(n1 % 10 + '0');
 }
