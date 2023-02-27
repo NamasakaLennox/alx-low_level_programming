453c622e72fa8e1a953aedaff16cbef13b07c697
NamasakaLennox <namasakalennox254@gmail.com>
2023-02-27 03:43:53 -0800

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * swap_int - swaps values of two variables
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: no value
+ */
+void swap_int(int *a, int *b)
+{
+	int i, j;
+
+	/* assign the values from the pointer to different variables */
+	i = *a;
+	j = *b;
+	/* swap the contents */
+	*a = j;
+	*b = i;
+}
