c1924eb66f78abd8af084741f0444c9a5bd27c31
Namasaka <lennoxnamasaka@gmail.com>
2023-03-16 13:56:04 +0300

    correction - memory fill

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,8 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i += size)
+	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
-
 	return (ptr);
 }
