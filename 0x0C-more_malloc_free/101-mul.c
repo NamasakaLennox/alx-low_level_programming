c1275a907fbd969d447d876f1e3f149daa28a1f1
Namasaka <lennoxnamasaka@gmail.com>
2023-03-16 23:27:05 +0300

    final changes

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
  * _calloc - calloc function
@@ -11,15 +10,15 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
 	unsigned int i;
+	char *ptr;
 
-	if (nmemb == 0 || size == 0)
+	if (size == 0 || nmemb == 0)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb * size; i++)
+	for (i = 0; i < nmemb * size; ++i)
 		ptr[i] = 0;
 	return (ptr);
 }
@@ -32,12 +31,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
  */
 int _strlen(char *s)
 {
-	int count;
+	int i;
 
-	count = 0;
-	while (s[count])
-		count++;
-	return (count);
+	for (i = 0; s[i]; ++i)
+		;
+	return (i);
 }
 
 /**
@@ -46,7 +44,7 @@ int _strlen(char *s)
  *
  * Return: no value
  */
-void _print(char *str)
+void _puts(char *str)
 {
 	while (*str)
 		_putchar(*(str++));
@@ -58,14 +56,17 @@ void _print(char *str)
  *
  * Return: 0 on success, 1 if fail
  */
-int _isdigit(char *s1)
+int _strdigit(char *s1)
 {
 	if (*s1 == '-' || *s1 == '+')
-		++s;
+		++s1;
 	while (*s1)
 	{
-		if (*s1 < 48 || *s1 > 57)
+		if (*s1 < '0' || *s1 > '9')
+		{
 			return (0);
+		}
+		++s1;
 	}
 	return (1);
 }
@@ -78,20 +79,21 @@ int _isdigit(char *s1)
  *
  * Return: no value
  */
-void reverse(int first, int last, char *str)
+void rev_num_str(int first, int last, char *str)
 {
 	int i, j;
-	char temp;
+	char tmp;
 
 	while (str[last] == 0 && last != first)
 		--last;
 	for (i = first, j = last; i <= j; ++i, --j)
 	{
-		temp = str[first];
-		str[first] = str[last];
-		str[last] = temp;
+		tmp = str[i] + '0';
+		str[i] = str[j] + '0';
+		str[j] = tmp;
 	}
 }
+
 /**
  * multiply - multiply a long string
  * @s1: string 1
@@ -99,7 +101,7 @@ void reverse(int first, int last, char *str)
  *
  * Return: pointer to answer
  */
-char *multiply(char *s1, char *s2)
+char *strmul(char *s1, char *s2)
 {
 	int len1, len2, i, j, a, b, neg = 0;
 	char *res;
@@ -138,7 +140,7 @@ char *multiply(char *s1, char *s2)
 		}
 		res[b] = sum_carry + mul_carry;
 	}
-	reverse(neg, b, res);
+	rev_num_str(neg, b, res);
 	return (res);
 }
 
@@ -155,21 +157,21 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		_print("Error\n");
+		_puts("Error\n");
 		exit(98);
 	}
-	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+	if (!_strdigit(argv[1]) || !_strdigit(argv[2]))
 	{
-		_print("Error\n");
+		_puts("Error\n");
 		exit(98);
 	}
-	ans = multiply(argv[1], argv[2]);
+	ans = strmul(argv[1], argv[2]);
 	if (ans == NULL)
 	{
-		_print("Error\n");
+		_puts("Error\n");
 		exit(98);
 	}
-	_print(ans);
+	_puts(ans);
 	_putchar('\n');
 	free(ans);
 	exit(EXIT_SUCCESS);
