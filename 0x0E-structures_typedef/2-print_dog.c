40f7cc6589366b655d0aaf9bb81ef2cd67c6f268
NamasakaLennox <namasakalennox254@gmail.com>
2023-03-20 17:33:42 +0300

    age output fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,10 +15,7 @@ void print_dog(struct dog *d)
 			printf("Name: (nil)\n");
 		else if (d->name)
 			printf("Name: %s\n", d->name);
-		if (!d->age)
-			printf("Age: (nil)\n");
-		else if (d->age)
-			printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 		if (!d->owner)
 			printf("Owner: (nil)\n");
 		else if (d->owner)
