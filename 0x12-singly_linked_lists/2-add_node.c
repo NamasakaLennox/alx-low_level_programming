86f13aa7c0f911a197c20eb1cfeeb6cc95e7c414
NamasakaLennox <namasakalennox254@gmail.com>
2023-03-30 13:40:42 +0300

    adds a new node at the beginning of a list

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * _strlen - gets the length of a string
+ * @str: string to check
+ *
+ * Return: the length of the string
+ */
+int _strlen(const char *str)
+{
+	int count = 0;
+
+	while (str[count])
+		count++;
+
+	return (count);
+}
+
+/**
+ * add_node - adds a node at the top of the linked list
+ * @head: the current top element
+ * @str: the element to add
+ *
+ * Return: pointer to the new head
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	new->str = (char *)str;
+	new->len = _strlen(str);
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
