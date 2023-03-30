ce9c2353937e4610d0f6ba143fb8bcb5057f94d3
NamasakaLennox <namasakalennox254@gmail.com>
2023-03-30 13:07:20 +0300

    returns number of elements in a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns number of elements in a linked list
+ * @h: address of the first element
+ *
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t nodes = 0;
+
+	while(h)
+	{
+		nodes++;
+		h = h->next;
+	}
+	return (nodes);
+}
