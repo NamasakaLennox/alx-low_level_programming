5e0cc880f1395ec54b63dc09b4f7104b18700a82
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-03 12:40:03 +0300

    [new:] deletes a node at index of a list

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at a certain index
+ * @head: address of the first element address
+ * @index: the element to delete
+ *
+ * Return: 1 on success, 0 on fail
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *previous = *head, *current;
+	unsigned int position = 0;
+
+	if (*head == NULL) /* if no element */
+	{
+		/* free_listint2(head); */
+		return (-1);
+	}
+	if (index == 0) /* deleting the first element */
+	{
+		current = *head;
+		*head = (*head)->next;
+		free(current);
+	}
+	else /* deleting element in any other position */
+	{
+		while (position < index - 1)
+		{
+			previous = previous->next;
+			position++;
+			if (previous == NULL) /* invalid index case */
+			{
+				free_listint2(head);
+				return (-1);
+			}
+		}
+		current = previous->next;
+		previous->next = current->next;
+		free(current);
+	}
+
+	return (1);
+}
