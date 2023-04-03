6679ec362f375cd4d88babf6f3fa7d6a1eb05ca2
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-03 19:08:47 +0300

    [update 2:] fix for conditional logic

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,25 +24,28 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	if (*head == NULL)
-	{
-		if (idx != 0)
-			return (NULL);
-		new->n = n;
-		new->next = (NULL);
-		return (new);
-	}
-
-	while (position < idx - 1) /* get to the postion to insert */
+	while (position < idx - 1 && next_elem) /* get to index to insert */
 	{
 		next_elem = next_elem->next;
 		position++;
-		if (next_elem == NULL && idx > position + 1) /* end of list */
-			return (NULL);
 	}
+
 	new->n = n;
-	new->next = next_elem->next;
-	next_elem->next = new;
+	if (idx == 0) /* start of list */
+	{
+		new->next = *head;
+		*head = new;
+	}
+	else if (position + 1 == idx) /* any other position */
+	{
+		new->next = next_elem->next;
+		next_elem->next = new;
+	}
+	else
+	{
+		free(new);
+		return (NULL);
+	}
 
 	return (new);
 }
