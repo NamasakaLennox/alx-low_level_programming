6c872cbbddeaabc554453ef6df617a5f55513291
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-07 19:52:31 +0300

    [new]: copies contents of a file to another file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,18 +30,18 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-
+	/* open file in write only mode */
 	f_open = open(filename, O_WRONLY || O_APPEND);
-	if (f_open == -1)
+	if (f_open < 0)
 		return (-1);
 
-	if (text_content)
+	if (text_content) /* write text if available */
 	{
 		char_print = write(f_open, text_content, _strlen(text_content));
-		if (char_print == -1)
+		if (char_print < 0)
 			return (-1);
 	}
-
+	/* close file and check for errors */
 	f_close = close(f_open);
 	if (f_close < 0)
 		return (-1);
