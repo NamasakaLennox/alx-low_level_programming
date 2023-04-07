e3d516f89f86715e26ea749b0afb539206794eaf
NamasakaLennox <namasakalennox254@gmail.com>
2023-04-07 17:26:12 +0300

    [update]: changed open permissions to write only

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -31,7 +31,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f_open = open(filename, O_APPEND || O_RDWR);
+	f_open = open(filename, O_WRONLY || O_APPEND);
 	if (f_open == -1)
 		return (-1);
 
