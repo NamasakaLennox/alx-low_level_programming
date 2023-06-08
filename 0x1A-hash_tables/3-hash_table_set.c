98d7d13925ca9d9d6e0bb97dbe63cb40f25f9c2e
NamasakaLennox <namasakalennox254@gmail.com>
2023-06-08 22:07:12 +0300

    edit: null value condition

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,6 +41,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	if (!key || key[0] == '\0')
 		return (0);
+	if (!value)
+		return(0);
 
 	/* get the index of the key */
 	index = key_index((const unsigned char *)key, ht->size);
