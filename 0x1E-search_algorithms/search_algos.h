6432b2dd1748eee93eb07c2f100a5b3943766773
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-11 13:40:27 +0300

    updated README.md file and the header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+#include <stdio.h>
+
+/* function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
