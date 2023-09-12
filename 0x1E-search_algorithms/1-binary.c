da5739b9e5237695f96cd164cec54ebae216ffc5
NamasakaLennox <namasakalennox254@gmail.com>
2023-09-12 10:13:15 +0300

    binary search bug fix

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t left = 0, right = size - 1, middle, i;
 
-	if (array == NULL || !(*array))
+	if (array == NULL)
 		return (-1);
 
 	while (left <= right)
